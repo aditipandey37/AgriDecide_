a126de849f5ab5921b4e2ef4271f6602a40085a3
Aditi Pandey <155900021+aditipandey37@users.noreply.github.com>
2026-05-13 18:02:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ with col2:
 # ---------- PAGE 3 ----------
 elif st.session_state.page == 3:
 
-   st.subheader("📊 Profit Comparison")
+   st.subheader(" Profit Comaprison ")
 
 q = st.session_state.quantity
 p = st.session_state.price
